45803469560ad055c14fdf251e3b1b4a2d8ae729
Eugene Krevenets <hyzhak@ya.ru>
2014-04-24 20:14:08 +0200

    create new instance of server on createServer

--- test/middlewareSpec.js
+++ test/middlewareSpec.js
@@ -5,10 +5,19 @@ describe('middleware', function() {
     'use strict';
 
     it('should be define', function() {
-        expect(middleware).to.be.not.undefined;
+        expect(middleware).to.not.be.undefined;
     });
 
     it('should has createServer function', function() {
         expect(middleware).to.have.property('createServer');
     });
+
+    it('should create new instance of servier on createServer', function() {
+        var instance1 = middleware.createServer(),
+            instance2 = middleware.createServer();
+
+        expect(instance1).to.not.be.undefined;
+        expect(instance2).to.not.be.undefined;
+        expect(instance1).to.not.be.equal(instance2);
+    })
 });
